33844e71808a9514b10731f2f188ab801be6b0bf
Bryan Scott <bryanbscott2000@gmail.com>
2016-03-05 09:38:45 -0500

    Started to defined the project and project tasks models. Must define a string base classes and thier properties.

---
+++ models.py
@@ -0,0 +1,24 @@
+from google.appengine.ext import ndb
+
+
+class Project(ndb.Model):
+    name = ndb.StringProperty()
+    description = ndb.TextProperty()
+    folder = ndb.StringProperty()
+    owner = ndb.StringProperty()
+    status = ndb.StringProperty()
+    created_on = ndb.DateTimeProperty(auto_now_add=True)
+    last_updated = ndb.DateTimeProperty(auto_now=True)
+
+
+class ProjectTasks(ndb.Model):
+    name = ndb.StringProperty()
+    summary = ndb.StringProperty()
+    estimated_hours = ndb.IntegerProperty()
+    estimated_start = ndb.DateProperty()
+    estimated_completion = ndb.DateProperty()
+    status = ndb.StringProperty()
+    project_key = ndb.IntegerProperty()
+    created_on = ndb.DateTimeProperty(auto_now_add=True)
+    last_updated = ndb.DateTimeProperty(auto_now=True)
+
